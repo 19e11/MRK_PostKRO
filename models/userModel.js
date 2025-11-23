34f96429b0450d2b819ec6ec9f46155546573942
schaturvedi0690@gmail.com <schaturvedi0690@gmail.com>
2025-11-23 20:47:10 +0530

    DBFixed

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectDB = mongoose.connect('mongodb://127.0.0.1:27017/MrkBlogger');
+const connectDB = mongoose.connect(process.env.MONGO_URL);
 
 const userSchema =new mongoose.Schema({
     name:{
